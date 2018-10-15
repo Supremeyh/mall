a8e262484cb6b262abfde99435e31154f8dded38
supremeyh <supremeyh@126.com>
2018-10-15 11:34:05 +0800

    10.15 父子通信

---
+++ demos/demo3/src/components/Child.vue
@@ -0,0 +1,29 @@
+<template> 
+    <div class="counter">
+        <p>子自: {{messenge}}</p>
+        <p>子: {{msg}}</p>
+        <button @click="say">change</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Child',
+        props: {
+            msg: {
+                type: String,
+                default: 'hi'
+            }
+        },
+        data(){
+            return {
+                messenge: 'child\'s own messenge'
+            }
+        },
+        methods:{
+            say(){
+                this.$emit('childsay', 'hello parent')
+            }
+        }
+    }
+</script>
---
+++ demos/demo3/src/components/Parent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+      <!--通过:msg将父组件的数据传递给子组件-->
+      <!--父组件监听子组件触发的childsay方法，调用自己的parentGet方法-->
+      <Child :msg="msgs" @childsay="parentGet"/>
+      <p>父: {{msgs}}</p>
+      <p>{{fromchild}}</p>
+  </div>
+</template>
+
+<script>
+import Child from './Child';
+export default {
+  name: 'Parent',
+  data () {
+    return {
+      msgs: 'hello child',
+      fromchild: ''
+    }
+  },
+  components: {
+    Child
+  },
+  methods: {
+    parentGet(msg){
+      this.fromchild = msg
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
