ac1e3c47f5e4cea43ff263884a79e85b7b4df98e
supremeyh <supremeyh@126.com>
2018-10-22 14:32:06 +0800

    10.22 商品列表组件拆分

---
+++ src/components/NavBread.vue
@@ -0,0 +1,10 @@
+<template>
+    <div ckass="nav-breadcrumb-wrap">
+        <div class="container">
+            <nav class="nav-breadcrumd">
+                <a href="/">Home</a>
+                <slot></slot>
+            </nav>
+        </div>
+    </div>
+</template>
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -1,11 +1,32 @@
 <template>
     <div class="goodlist">
-        goodlist
+        <nav-header></nav-header>
+        <nav-bread>
+            <span>Goods</span>
+        </nav-bread>
+        <nav-footer></nav-footer>
     </div>
 </template>
 
 <script>
     import '../assets/css/base.css'
     import '../assets/css/product.css'
+    import NavHeader from '../components/NavHeader.vue';
+    import NavFooter from '../components/NavFooter.vue';
+    import NavBread from '../components/NavBread.vue';
+
+    export default{
+        name: 'GoodsList',
+        data(){
+           return {
+
+           }
+        },
+        components: { 
+            NavHeader,
+            NavFooter,
+            NavBread 
+        }
+    }
     
 </script>
